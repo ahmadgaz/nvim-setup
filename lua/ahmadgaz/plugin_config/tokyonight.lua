b974ff470ceeb2ec2820a7dffc63fe665a790639
ahmadgaz <gazali.contact@gmail.com>
2024-04-06 15:12:47 -0700

    switch to lazy.nvim

---
+++ lua/ahmadgaz/plugin_config/tokyonight.lua
@@ -0,0 +1,6 @@
+require("tokyonight").setup({
+    style = "storm",    -- The theme comes in three styles, `storm`, `moon`, a darker variant `night` and `day`
+    transparent = true, -- Enable this to disable setting the background color
+})
+
+vim.cmd('colorscheme tokyonight')
